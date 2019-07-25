0f71a131f003e7efbe3124a160ff2250c0ccc11e
MaJesTySA <39079548+MaJesTySA@users.noreply.github.com>
2019-07-26 00:25:30 +0800

    Create SemaphoreDemo.java

---
+++ src/thread/SemaphoreDemo.java
@@ -0,0 +1,26 @@
+package thread;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class SemaphoreDemo {
+    public static void main(String[] args) {
+        Semaphore semaphore=new Semaphore(3);
+        for (int i = 1; i <=6 ; i++) {
+            new Thread(()->{
+                try {
+                    semaphore.acquire();
+                    System.out.println(Thread.currentThread().getName()+
+                            "\t抢到车位");
+                    try{ TimeUnit.SECONDS.sleep(3); }catch (Exception e){ e.printStackTrace(); }
+                    System.out.println(Thread.currentThread().getName()+
+                            "\t停车3秒后离开车位");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    semaphore.release();
+                }
+            },String.valueOf(i)).start();
+        }
+    }
+}
