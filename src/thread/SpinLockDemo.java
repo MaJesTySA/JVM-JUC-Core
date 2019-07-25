a03ad740039f189d6b3b636dffd529b2fc009ac6
MaJesTySA <39079548+MaJesTySA@users.noreply.github.com>
2019-07-25 23:52:01 +0800

    Create SpinLockDemo.java

---
+++ src/thread/SpinLockDemo.java
@@ -0,0 +1,49 @@
+package thread;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class SpinLockDemo {
+    AtomicReference<Thread> atomicReference = new AtomicReference<>();
+
+    public static void main(String[] args) {
+        SpinLockDemo spinLockDemo = new SpinLockDemo();
+        new Thread(() -> {
+            spinLockDemo.myLock();
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            spinLockDemo.myUnlock();
+        }, "AA").start();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        new Thread(() -> {
+            spinLockDemo.myLock();
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            spinLockDemo.myUnlock();
+        }, "BB").start();
+    }
+
+    public void myLock() {
+        Thread thread = Thread.currentThread();
+        System.out.println(Thread.currentThread().getName() + "\t" + " come in ...");
+        while (!atomicReference.compareAndSet(null, thread)) { }
+    }
+
+    public void myUnlock() {
+        Thread thread = Thread.currentThread();
+        atomicReference.compareAndSet(thread, null);
+        System.out.println(Thread.currentThread().getName() + "\t" + " unlock ...");
+    }
+}
