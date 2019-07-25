e2db1938d77916c59113ce8400704aa99a63271b
MaJesTySA <39079548+MaJesTySA@users.noreply.github.com>
2019-07-26 00:17:53 +0800

    Create CountryEnum.java

---
+++ src/thread/CountryEnum.java
@@ -0,0 +1,37 @@
+package thread;
+
+public enum CountryEnum {
+    ONE(1, "齐"), TWO(2, "楚"), THREE(3, "燕"), FOUR(4, "赵"), FIVE(5, "魏"), SIX(6, "韩");
+    private Integer retCode;
+    private String retMsg;
+
+    CountryEnum(Integer retCode, String retMsg) {
+        this.retCode = retCode;
+        this.retMsg = retMsg;
+    }
+
+    public Integer getRetCode() {
+        return retCode;
+    }
+
+    public void setRetCode(Integer retCode) {
+        this.retCode = retCode;
+    }
+
+    public String getRetMsg() {
+        return retMsg;
+    }
+
+    public void setRetMsg(String retMsg) {
+        this.retMsg = retMsg;
+    }
+
+    public static CountryEnum list(int idx) {
+        CountryEnum[] countryEnums = CountryEnum.values();
+        for (CountryEnum countryEnum : countryEnums) {
+            if (idx==countryEnum.getRetCode())
+                return countryEnum;
+        }
+        return null;
+    }
+}
