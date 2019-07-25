2b5d8b40fb71d5e82315752e8d94264e1c20d2c1
MaJesTySA <39079548+MaJesTySA@users.noreply.github.com>
2019-07-26 00:15:55 +0800

    Create CountDownLatchDemo.java

---
+++ src/thread/CountDownLatchDemo.java
@@ -0,0 +1,34 @@
+package thread;
+
+import java.util.concurrent.CountDownLatch;
+
+public class CountDownLatchDemo {
+    public static void main(String[] args) throws InterruptedException {
+        leaveClassroom();
+        county();
+    }
+
+    private static void county() throws InterruptedException {
+        CountDownLatch countDownLatch=new CountDownLatch(6);
+        for (int i = 1; i <=6 ; i++) {
+            new Thread(()->{
+                System.out.println(Thread.currentThread().getName()+"\t 国被灭");
+                countDownLatch.countDown();
+            }, CountryEnum.list(i).getRetMsg()).start();
+        }
+        countDownLatch.await();
+        System.out.println(Thread.currentThread().getName()+"\t ******秦国一统华夏");
+    }
+
+    private static void leaveClassroom() throws InterruptedException {
+        CountDownLatch countDownLatch=new CountDownLatch(6);
+        for (int i = 1; i <=6 ; i++) {
+            new Thread(()->{
+                System.out.println(Thread.currentThread().getName()+"\t上完自习，离开教室");
+                countDownLatch.countDown();
+            },String.valueOf(i)).start();
+        }
+        countDownLatch.await();
+        System.out.println(Thread.currentThread().getName()+"\t ******班长最后关门走人");
+    }
+}
