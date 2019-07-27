8f7467ec678b7933dffb3b9349f1a8febd36b927
MaJesTySA <39079548+MaJesTySA@users.noreply.github.com>
2019-07-27 17:26:01 +0800

    Create DeadLockDemo.java

---
+++ src/thread/DeadLockDemo.java
@@ -0,0 +1,37 @@
+package thread;
+
+import java.util.concurrent.TimeUnit;
+
+public class DeadLockDemo {
+    public static void main(String[] args) {
+        String lockA = "lockA";
+        String lockB = "lockB";
+        new Thread(new HoldLockThread(lockA, lockB), "ThreadA").start();
+        new Thread(new HoldLockThread(lockB, lockA), "ThreadB").start();
+    }
+}
+
+class HoldLockThread implements Runnable {
+    private String lockA;
+    private String lockB;
+
+    public HoldLockThread(String lockA, String lockB) {
+        this.lockA = lockA;
+        this.lockB = lockB;
+    }
+
+    @Override
+    public void run() {
+        synchronized (lockA) {
+            System.out.println(Thread.currentThread().getName() + "\t自己持有：" + lockA + "\t尝试获取：" + lockB);
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            synchronized (lockB) {
+                System.out.println(Thread.currentThread().getName() + "\t自己持有：" + lockB + "\t尝试获取：" + lockA);
+            }
+        }
+    }
+}
