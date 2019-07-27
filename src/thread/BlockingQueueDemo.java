8f83dab5bcb70f81e50ed850a68071ab7dddfe32
MaJesTySA <39079548+MaJesTySA@users.noreply.github.com>
2019-07-27 10:15:10 +0800

    Create BlockingQueueDemo.java

---
+++ src/thread/BlockingQueueDemo.java
@@ -0,0 +1,57 @@
+package thread;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class BlockingQueueDemo {
+    public static void main(String[] args) throws InterruptedException {
+        BlockingQueue<String> blockingQueue = new ArrayBlockingQueue<String>(3);
+        addAndRemove(blockingQueue);
+        offerAndPoll(blockingQueue);
+        putAndTake(blockingQueue);
+        outOfTime(blockingQueue);
+    }
+
+    private static void outOfTime(BlockingQueue<String> blockingQueue) throws InterruptedException {
+        System.out.println(blockingQueue.offer("a",2L, TimeUnit.SECONDS));
+        System.out.println(blockingQueue.offer("a",2L, TimeUnit.SECONDS));
+        System.out.println(blockingQueue.offer("a",2L, TimeUnit.SECONDS));
+        System.out.println(blockingQueue.offer("a",2L, TimeUnit.SECONDS));
+    }
+
+    private static void putAndTake(BlockingQueue<String> blockingQueue) throws InterruptedException {
+        blockingQueue.put("a");
+        blockingQueue.put("b");
+        blockingQueue.put("c");
+        blockingQueue.put("d");
+        System.out.println(blockingQueue.take());
+        System.out.println(blockingQueue.take());
+        System.out.println(blockingQueue.take());
+        System.out.println(blockingQueue.take());
+    }
+
+    private static void offerAndPoll(BlockingQueue<String> blockingQueue) {
+        System.out.println(blockingQueue.offer("a"));
+        System.out.println(blockingQueue.offer("b"));
+        System.out.println(blockingQueue.offer("c"));
+        System.out.println(blockingQueue.offer("e"));
+        System.out.println(blockingQueue.peek());
+        System.out.println(blockingQueue.poll());
+        System.out.println(blockingQueue.poll());
+        System.out.println(blockingQueue.poll());
+        System.out.println(blockingQueue.poll());
+    }
+
+    private static void addAndRemove(BlockingQueue<String> blockingQueue) {
+        System.out.println(blockingQueue.add("a"));
+        System.out.println(blockingQueue.add("b"));
+        System.out.println(blockingQueue.add("c"));
+        System.out.println(blockingQueue.add("e"));
+        System.out.println(blockingQueue.element());
+        System.out.println(blockingQueue.remove());
+        System.out.println(blockingQueue.remove());
+        System.out.println(blockingQueue.remove());
+        System.out.println(blockingQueue.remove());
+    }
+}
