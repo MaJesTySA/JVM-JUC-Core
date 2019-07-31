2b4a6627276d3ec3ce097fb57906c2cd708ae4a3
MaJesTySA <39079548+MaJesTySA@users.noreply.github.com>
2019-07-31 23:23:24 +0800

    Create StackOverflowErrorDemo.java

---
+++ src/jvm/StackOverflowErrorDemo.java
@@ -0,0 +1,12 @@
+package jvm;
+
+public class StackOverflowErrorDemo {
+    public static void main(String[] args) {
+        stackOverflowError();
+    }
+
+    private static void stackOverflowError() {
+        stackOverflowError();
+    }
+
+}
