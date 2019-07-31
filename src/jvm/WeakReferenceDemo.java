ee6e2284f25955bdb7cf8704ccb237c3cd7407fe
MaJesTySA <39079548+MaJesTySA@users.noreply.github.com>
2019-07-31 22:57:16 +0800

    Create WeakReferenceDemo.java

---
+++ src/jvm/WeakReferenceDemo.java
@@ -0,0 +1,17 @@
+package jvm;
+
+import java.lang.ref.WeakReference;
+
+public class WeakReferenceDemo {
+    public static void main(String[] args) {
+        Object o1 = new Object();
+        WeakReference<Object> weakReference = new WeakReference<>(o1);
+        System.out.println(o1);
+        System.out.println(weakReference.get());
+        System.out.println("==========");
+        o1 = null;
+        System.gc();
+        System.out.println(o1);
+        System.out.println(weakReference.get());
+    }
+}
